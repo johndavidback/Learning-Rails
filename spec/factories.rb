80b6550cff4885fbff855304883511c57a8c0e15
John David Back <johndavidback@gmail.com>
2012-02-10 13:49:55 -0500

    Finished the user signup

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+	factory :user do
+		name		"John Back"
+		email		"john@back.com"
+		password "foobar"
+	end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,11 +1,37 @@
 require 'spec_helper'
 
-describe "UserPages" do
-  describe "GET /user_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get user_pages_index_path
-      response.status.should be(200)
+describe "User Pages" do
+  subject { page }
+
+  describe "signup" do
+  	before { visit signup_path }
+
+  	describe "with valid information" do
+      it "should not createa a user" do
+        expect { click_button "Sign up" }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "John Back"
+        fill_in "Email",        with: "john@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar" 
+      end
+
+      it "should create a user" do
+        expect { click_button "Sign up" }.to change(User, :count).by(1)
+      end
     end
   end
+
+  describe "signup page" do
+  	let(:user) { FactoryGirl.create(:user) }
+  	before { visit user_path(user) }
+
+  	it { should have_selector('h1', text: user.name ) }
+  	it { should have_selector('title', text: user.name ) }
+  end
+
 end
