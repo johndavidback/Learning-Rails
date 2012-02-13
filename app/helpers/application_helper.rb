0489a238006ab8e55e44c3bb702a4cda8ad01106
John David Back <johndavidback@gmail.com>
2012-02-13 10:50:54 -0500

    Finishing the sign in, however the tests fail

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+	def logo
+		image_tag('logo.png', alt: 'The sample, son', class: 'round')
+	end
+
 	# Returns the full title on a per-page basis?
 	def full_title(page_title)
 		base_title = "Rails Tutorial"
